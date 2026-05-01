f1184056001071ae340d8f6bf3eb77e0807dbae4
EfraimDeveloper <efraimprogram@gmail.com>
2026-05-01 12:52:42 +0100

    test deploy1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
   <h1>Welcome to My Portfolio GitHub and Deployment ou ci/cd-- azure devop 1</h1>
     <h3 style={{ color: 'red' }}>Efraim luis marcalino</h3>
+
+    <h1>TESTE NOVO DO DEPLOY 123</h1>
       </div>
   );
 }
