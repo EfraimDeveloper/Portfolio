b75bc357baca2703443652468317591b10325745
EfraimDeveloper <efraimprogram@gmail.com>
2026-05-02 11:00:35 +0100

    change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,48 @@
 import React from "react";
 
-const Navbar=()=>{
-    return(
-        <div className="navbar">
-           <a href="#Home">Home</a>|
-           <a href="#Projects">Projects</a>|
-           <a href="#About">About</a>|
-           <a href="#Blog">Blog</a>|
-           <a href="#CV">CV</a>|
-           <a href="#Contact">Contact</a>
-        </div>
-    )
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+      
+      <a className="navbar-brand fw-bold" href="#home">
+        MyPortfolio
+      </a>
+
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto gap-2">
+
+          <li className="nav-item">
+            <a className="nav-link" href="#home">Home</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#projects">Projects</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#about">About</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#blog">Blog</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#cv">CV</a>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link" href="#contact">Contact</a>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
